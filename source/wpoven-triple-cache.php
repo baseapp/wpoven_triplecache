6759f1813688c4907f25989552c20f2b9f897927
shyambaseapp <shyam.baseapp@gmail.com>
2024-11-12 16:58:57 +0530

    plugin update checker git repo url

--- source/wpoven-triple-cache.php
+++ source/wpoven-triple-cache.php
@@ -79,7 +79,7 @@ require_once plugin_dir_path(__FILE__) . 'includes/libraries/plugin-update-check
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/baseapp/wpoven_triplecache.git',
+	'https://github.com/baseapp/wpoven_triplecache/',
 	__FILE__,
 	'wpoven-triple-cache'
 );
